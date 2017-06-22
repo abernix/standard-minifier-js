16f5c2c55ba3483e945925fc1856b65f28909826
Jesse Rosenberger <git@jro.cc>
2017-06-22 11:06:04 +0300

    Bump version to 2.1.0 for non-beta release.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'abernix:standard-minifier-js',
-  version: '2.1.0-beta.0',
+  version: '2.1.0',
   summary: 'Harmony javascript minifiers used with Meteor apps by default.',
   documentation: 'README.md',
 });
@@ -8,7 +8,7 @@ Package.describe({
 Package.registerBuildPlugin({
   name: "minifyStdJS",
   use: [
-    'abernix:minifier-js@2.1.0-beta.0',
+    'abernix:minifier-js@2.1.0',
     'babel-compiler@6.19.2',
     'ecmascript@0.8.0'
   ],
